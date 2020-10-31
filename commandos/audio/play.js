73e8d0c47ffe0e669bbab9d799ca68baec69777d
FifcoStyle <fifcostyle@gmail.com>
2020-10-31 14:05:00 +0100

    restrict play during lessons

--- commandos/audio/play.js
+++ commandos/audio/play.js
@@ -36,6 +36,9 @@ module.exports = class AudioCommand extends commando.Command {
 	}
 	async run(message, args) {
 		if (message.member.voice.channel) {
+			const lesson = this.client.lessons.find(les => les.class === message.member.voice.channel.name.slice(0, 2));
+			if (lesson && message.member != lesson.teacher) return message.reply(`Only the teacher can play audio during the lesson!`).then(res => {res.delete({ timeout: 5000 }); message.delete({ timeout: 5000 });});
+
 			if (args.type === `youtube`) {
 				const connection = await message.member.voice.channel.join();
 				const dispatcher = connection.play(ytdl(args.loc, { filter: "audioonly" }), { volume: args.vol / 10 });
