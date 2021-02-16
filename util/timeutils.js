fd85a4d4a382107d137da2501ffcb634b892cd8c
AntalaFilip <fifcostyle@gmail.com>
2021-02-16 07:51:18 +0100

    timeutils now return null period instead of 0

--- util/timeutils.js
+++ util/timeutils.js
@@ -1,11 +1,11 @@
 const TimeUtils = {
 	/**
-	 * @returns {number} returns a number representing a period
+	 * @returns {number | null} returns a number representing a period
 	 */
 	getCurrentPeriod() {
 		const date = new Date();
 
-		if (date.getHours() <= 8 || date.getHours() >= 13) return 0;
+		if (date.getHours() <= 8 || date.getHours() >= 14) return null;
 
 		return date.getHours() - 8;
 	},
