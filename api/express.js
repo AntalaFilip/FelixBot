3d9c652cd7f1a6b5fe08fff1a045ee0800e53104
AntalaFilip <fifcostyle@gmail.com>
2021-02-22 17:19:58 +0100

    added auth rate limiter

--- api/express.js
+++ api/express.js
@@ -5,13 +5,19 @@ const lessonHandler = require('./lessonhandler');
 const authHandler = require('./auth').router;
 const authorizer = require('./auth').reqauth;
 const cors = require('cors');
+const RateLimit = require('express-rate-limit');
+
+const authLimiter = new RateLimit({
+	windowMs: 60 * 1000,
+	max: 2,
+});
 
 app.use(authorizer);
 app.use(cors({ origin: '*' }));
 
 app.use('/lessons', lessonHandler);
 
-app.use('/auth', authHandler);
+app.use('/auth', authLimiter, authHandler);
 
 app.get('/bot', (req, res) => {
 	res.send(global.client.user);
