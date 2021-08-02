57a37d672e6ccb1973a429f5b038f3362dc46930
Filip Antala <filip.antala@felixmuzikal.sk>
2021-08-02 19:37:16 +0200

    use subject shorts instead of whole names

--- util/timetable.js
+++ util/timetable.js
@@ -13,7 +13,7 @@ class TimetableUtil {
 				card => {
 					// TODO add check if card contains two groups with same name and if so, add classes to clarify them
 					// TODO? possibly make it into mentions instead of plains names
-					const str = `${card.period.starttime}-${card.period.endtime} > ${card.lesson.subject.name} with ${card.lesson.teachers.map(t => t.member ? removeStartingDot(t.member.displayName) : t.short).join(' & ')} group ${card.lesson.groups.map(g => g.name).join(' & ')}`;
+					const str = `${card.period.starttime}-${card.period.endtime} > ${card.lesson.subject.short} with ${card.lesson.teachers.map(t => t.member ? removeStartingDot(t.member.displayName) : t.short).join(' & ')} group ${card.lesson.groups.map(g => g.name).join(' & ')}`;
 					return [
 						card.id,
 						str,
