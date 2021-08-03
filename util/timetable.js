2b01d70d6fa84aa6669205e26e6e25c534188d87
Filip Antala <filip.antala@felixmuzikal.sk>
2021-08-03 09:46:25 +0200

    readability and group names uniqueness

--- util/timetable.js
+++ util/timetable.js
@@ -11,9 +11,19 @@ class TimetableUtil {
 		const collection = new Collection(
 			cards.map(
 				card => {
-					// TODO add check if card contains two groups with same name and if so, add classes to clarify them
 					// TODO? possibly make it into mentions instead of plains names
-					const str = `${card.period.starttime}-${card.period.endtime} > ${card.lesson.subject.short} with ${card.lesson.teachers.map(t => t.member ? removeStartingDot(t.member.displayName) : t.short).join(' & ')} group ${card.lesson.groups.map(g => g.name).join(' & ')}`;
+					const short = card.lesson.subject.short;
+					const time = `${card.period.starttime}-${card.period.endtime}`;
+					// Map the teachers' names - if they have already identified in Discord, use their Discord member name, else use the EduPage short
+					const tnames = card.lesson.teachers.map(t => t.member ? removeStartingDot(t.member.displayName) : t.short).join('&');
+					// Map the group names
+					const garr = card.lesson.groups.map(g => g.name);
+					// Check if the group names are unique
+					const isGUnique = garr.every((g, i, a) => a.indexOf(g, i + 1) === -1);
+					// If they are not unique, remap the group names with class names
+					if (!isGUnique) garr.splice(0, garr.length, ...card.lesson.groups.map(g => `${g.name} (${g.class.name})`));
+					const gnames = garr.join(' & ');
+					const str = `${time} > ${short} with ${tnames} group ${gnames}`;
 					return [
 						card.id,
 						str,
