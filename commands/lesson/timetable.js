2230e00b20321f9fceacffbcdeb2ddcb229d5350
Filip Antala <filip.antala@felixmuzikal.sk>
2021-08-04 09:17:06 +0200

    reworked timetable command to work with new changes

--- commands/lesson/timetable.js
+++ commands/lesson/timetable.js
@@ -1,8 +1,12 @@
-const { MessageEmbed, CommandInteraction, MessageActionRow, MessageSelectMenu, Message, MessageComponentInteraction } = require("discord.js");
-const timetable = require(`../../timetable`);
+const { MessageEmbed, CommandInteraction, MessageActionRow, MessageSelectMenu, Message, MessageComponentInteraction, GuildMember, Role } = require("discord.js");
 const { Command } = require("../../types/command");
-const { getChanName, removeStartingDot } = require("../../util/stringutils");
+const { getChanName, removeStartingDot: rsd } = require("../../util/stringutils");
 const config = require('../../config.json');
+const EduStudent = require("../../types/edu/edustudent");
+const EduTeacher = require("../../types/edu/eduteacher");
+const EduClass = require("../../types/edu/educlass");
+const TimetableUtil = require("../../util/timetable");
+const EduBase = require("../../types/edu/edubase");
 
 class TimetableCommand extends Command {
 	constructor(client) {
@@ -17,7 +21,12 @@ class TimetableCommand extends Command {
 		});
 
 		this.components = {
-			timetableSelect: (member, roles) => (
+			/**
+			 * @param {GuildMember} member
+			 * @param {EduBase[]} edu
+			 * @returns
+			 */
+			timetableSelect: (member, edu, def) => (
 				new MessageSelectMenu({
 					placeholder: 'Select class:',
 					minValues: 1,
@@ -25,10 +34,11 @@ class TimetableCommand extends Command {
 					customId: `timetable_select_class/${member.id}`,
 				})
 					.addOptions(
-						roles.map(r => ({
-							label: r.name,
-							value: r.name.toLowerCase(),
-						})),
+						edu.map(e => ({
+							label: (e.member && rsd(e.member.displayName)) || e.name || e.short,
+							value: e.id,
+							default: def && def === e.id,
+						})).reverse(),
 					)
 			),
 		};
@@ -45,106 +55,139 @@ class TimetableCommand extends Command {
 		const member = interaction.member;
 		const chan = interaction.channel;
 		const args = interaction.options;
-		const cls = args.get('class');
-		const silent = args.get('silent');
+		const target = args.getMentionable('target') || member;
+		const silent = args.getBoolean('silent');
+		const EDU = this.client.edupageManager;
 
-		let clsid = cls && cls.value.slice(0, 2);
-		if (!clsid) clsid = getChanName(chan).slice(0, 2);
-		const role = guild.roles.cache.find(r => r.name.toLocaleLowerCase().startsWith(clsid));
-		if (!role) return await interaction.reply({ ephemeral: true, content: `Invalid class, try specifying a class in the command!` });
+		const edu = (target instanceof Role)
+			? EDU.classes.find(c => c.role === target)
+			: (EDU.teachers.find(t => t.member && t.member === target) || EDU.students.find(s => s.member && s.member === target));
 
-		const embed = await this.exec(member, role, clsid);
+		if (!edu) {
+			if (target instanceof Role) return await interaction.reply({ ephemeral: true, content: `This role does not have a timetable!` });
+			else return await interaction.reply({ ephemeral: true, content: `This user is not linked to an EduPage user!` });
+		}
 
-		/** @type {boolean} */
-		const ephemeral = (silent && silent.value) ?? true;
-		const roles = guild.roles.cache.filter(r => config.classRoles.find(cr => cr.value === r.id));
+		const embed = await this.exec(edu, member);
+
+		const ephemeral = silent ?? true;
+		const classes = EDU.classes;
+		const me = EDU.teachers.find(t => t.member === member) || EDU.students.find(s => s.member === member);
+		const choices = classes.concat(me).filter(o => o);
 		await interaction.reply({
 			ephemeral: ephemeral,
 			embeds: [embed],
 			components: [
 				new MessageActionRow()
 					.addComponents(
-						this.components.timetableSelect(member, roles),
+						this.components.timetableSelect(member, choices, edu.id),
 					),
 			],
 		});
 	}
 
-	async exec(member, role, clsid) {
-		const guild = member.guild;
-		if (!role) role = guild.roles.cache.find(r => r.name.toLocaleLowerCase().startsWith(clsid));
+	/**
+	 * @param {EduStudent | EduTeacher | EduClass} edu
+	 * @param {GuildMember} member
+	 * @returns
+	 */
+	async exec(edu, member) {
+		const EDU = this.client.edupageManager;
+		const cards = EDU.cards.filter(c => c.lesson.teacherids.includes(edu.id) || c.lesson.studentids.includes(edu.id) || c.lesson.classids.includes(edu.id));
+		/** @type {string} */
+		const name = (edu.member && rsd(edu.member.displayName)) || edu.name || edu.short;
+		/** @type {Role | GuildMember} */
+		const mention = (edu instanceof EduClass) ? edu.role : edu.member;
+		const mtext = mention && `<@${mention instanceof Role ? '&' : ''}${mention.id}>`;
 		const embed = new MessageEmbed()
-			.setColor(`#000000`)
-			.setTitle(`Timetable for ${role.name}`)
+			.setColor(`FUCHSIA`)
+			.setTitle(`Timetable for ${name}`)
 			.setURL(`https://felix.edupage.org/timetable/`)
 			.setAuthor(this.client.user.username, this.client.user.avatarURL())
-			.setDescription(`This is the timetable for <@&${role.id}>, requested by <@${member.id}>`)
+			.setDescription(`${mtext || name}'s timetable, requested by <@${member.id}>`)
 			.setThumbnail(`https://cdn.discordapp.com/attachments/371283762853445643/768906541277380628/Felix-logo-01.png`)
-			.setFooter(timetable[0][0])
+			.setFooter(`Timetable - ${EDU.raw[0].data_rows[0].reg_name} - version ${EDU.currenttt.text}`)
 			.setTimestamp();
 
-		for (let i = 0; i < timetable.length; i++) {
-			const processed = new Array();
-			const filtered = timetable[i].filter(el => el.includes(`@${clsid}`) || el.includes(`&${clsid}`));
-			if (filtered.length == 0) processed.push(`No lessons!`);
-			filtered.forEach((el, ii) => {
-				const lesson = el.substring(1, el.indexOf(`@`));
-				const teacher = guild.members.resolve(el.substring(el.indexOf(`#`) + 1, el.indexOf(`$`)));
-				const group = el.substring(el.indexOf(`$`) + 1, el.indexOf(`%`));
-				const period = el.substring(el.indexOf(`%`) + 1, el.indexOf(`^`));
-				if (!lesson || !teacher || !group || !period) {
-					this.client.logger.warn(`Invalid timetable entry! ${i}:${ii}:${clsid}`);
-					processed.push(`INVALID ENTRY: ${el}`);
-					return;
-				}
-				processed.push(`${period}: ${lesson.toUpperCase()} with ${removeStartingDot(teacher.displayName)} group ${group.toUpperCase()}`);
+		const textcards = TimetableUtil.cardsToText(cards, edu instanceof EduTeacher).sort(
+			(_, __, c1, c2) => {
+				const card1 = EDU.cards.find(c => c.id === c1);
+				const card2 = EDU.cards.find(c => c.id === c2);
+				return card1.period.period - card2.period.period;
 			});
-			switch (i) {
-				case 1:
-					embed.addField(`Monday`, processed.join('\n'));
-					break;
-				case 2:
-					embed.addField(`Tuesday`, processed.join('\n'));
-					break;
-				case 3:
-					embed.addField(`Wednesday`, processed.join('\n'));
-					break;
-				case 4:
-					embed.addField(`Thursday`, processed.join('\n'));
-					break;
-				case 5:
-					embed.addField(`Friday`, processed.join('\n'));
-					break;
+		const mappedcards = textcards.map(
+			(text, id) => {
+				const card = EDU.cards.find(c => c.id === id);
+				const days = EDU.daydefs.filter(dd => dd.matches(card.rawdays)).map(dd => dd.day).filter(o => o);
+				const weeks = EDU.weekdefs.filter(wd => wd.matches(card.rawweeks)).map(wd => wd.week).filter(o => o);
+				text = `${card.period.name}: ${text}`;
+				return [text, days, weeks];
+			});
+
+		const filtered = mappedcards.filter(([_, __, w]) => w.includes(EDU.currentWeek));
+		const timetable = [];
+		for (let i = 0; i < EDU.days.length; i++) timetable.push([]);
+
+		for (const card of filtered) {
+			for (const day of card[1]) {
+				const d = Number(day.id);
+				timetable[d].push(card[0]);
 			}
 		}
+
+		embed.addFields(
+			timetable.map(
+				(t, i) => ({
+					name: EDU.days.find(d => d.id == i).name,
+					value: t.join('\n'),
+				}),
+			),
+		);
 
 		return embed;
 	}
 
 	/**
 	 *
-	 * @param {string} rawid
 	 * @param {MessageComponentInteraction} interaction
 	 * @returns
 	 */
-	async component(rawid, interaction) {
-		const split = rawid.split('/');
+	async component(interaction) {
+		const split = interaction.customId.split('/');
 		const id = split[0];
 		const args = split.slice(1);
 		if (id === `timetable_select_class` && interaction.isSelectMenu()) {
+			const EDU = this.client.edupageManager;
 			const member = interaction.member;
 			const isOriginalAuthor = (args[0] == member.id);
-			const cls = interaction.values[0];
-			const clsid = cls.slice(0, 2);
+			const eduid = interaction.values[0];
+			const edu = EDU.classes.find(c => c.id === eduid) || EDU.teachers.find(t => t.id === eduid) || EDU.students.find(s => s.id === eduid);
 
-			const embed = await this.exec(member, null, clsid);
+			const classes = EDU.classes;
+			const me = EDU.teachers.find(t => t.member === member) || EDU.students.find(s => s.member === member);
+			const choices = classes.concat(me).filter(o => o);
+
+			const embed = await this.exec(edu, member);
 
 			if (isOriginalAuthor) {
-				await interaction.update({ embeds: [embed] });
+				await interaction.update({
+					embeds: [embed],
+					components: [
+						new MessageActionRow()
+							.addComponents(this.components.timetableSelect(member, choices, edu.id)),
+					],
+				});
 				return;
 			}
 			else {
-				await interaction.reply({ ephemeral: true, embeds: [embed] });
+				await interaction.reply({
+					ephemeral: true,
+					embeds: [embed],
+					components: [
+						new MessageActionRow()
+							.addComponents(this.components.timetableSelect(member, choices, edu.id)),
+					],
+				});
 				return;
 			}
 		}
