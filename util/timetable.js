afae6ce18f06bfe7e55742a7dc627fa87b6df512
Filip Antala <filip.antala@felixmuzikal.sk>
2021-08-04 09:16:57 +0200

    added formatting for teacher timetables and added mentions

--- util/timetable.js
+++ util/timetable.js
@@ -6,24 +6,29 @@ const { removeStartingDot } = require("./stringutils");
 class TimetableUtil {
 	/**
 	 * @param {EduCard[]} cards
+	 * @param {boolean} teacher
 	 */
-	static cardsToText(cards) {
+	static cardsToText(cards, teacher) {
 		const collection = new Collection(
 			cards.map(
 				card => {
-					// TODO? possibly make it into mentions instead of plains names
-					const short = card.lesson.subject.short;
+					const short = card.lesson.subject.role ? `<@&${card.lesson.subject.role.id}>` : card.lesson.subject.short;
 					const time = `${card.period.starttime}-${card.period.endtime}`;
-					// Map the teachers' names - if they have already identified in Discord, use their Discord member name, else use the EduPage short
-					const tnames = card.lesson.teachers.map(t => t.member ? removeStartingDot(t.member.displayName) : t.short).join('&');
+					// Map the teachers' names - if they have already identified in Discord, mention them, else use the EduPage short
+					const tnames = card.lesson.teachers.map(t => t.member ? `<@${t.member.id}>` : t.short).join(' & ');
+					// Map the classes' names - if they have an assigned role mention id, else use the plaintext name
+					const cnames = card.lesson.classes.map(c => c.role ? `<@&${c.role.id}>` : c.name).join(' & ');
 					// Map the group names
 					const garr = card.lesson.groups.map(g => g.name);
 					// Check if the group names are unique
 					const isGUnique = garr.every((g, i, a) => a.indexOf(g, i + 1) === -1);
 					// If they are not unique, remap the group names with class names
-					if (!isGUnique) garr.splice(0, garr.length, ...card.lesson.groups.map(g => `${g.name} (${g.class.name})`));
+					if (!isGUnique) garr.splice(0, garr.length, ...card.lesson.groups.map(g => `${g.name} (${g.class.role ? '<@&' + g.class.role.id + '>' : g.class.name})`));
+					// join into a string
 					const gnames = garr.join(' & ');
-					const str = `${time} > ${short} with ${tnames} group ${gnames}`;
+					// create main string
+					const str = `${time} > ${short} with ${teacher ? cnames : tnames} group ${gnames}`;
+					// return in collection-accepted format
 					return [
 						card.id,
 						str,
