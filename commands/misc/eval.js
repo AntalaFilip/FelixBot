130c01906d465ab760b6ba8f04aa8307d8f3abd6
Filip Antala <filip.antala@felixmuzikal.sk>
2021-11-13 14:08:25 +0100

    added Eval command

---
+++ commands/misc/eval.js
@@ -0,0 +1,49 @@
+const { Command } = require("../../types/command");
+const config = require('../../config.json');
+
+class EvalCommand extends Command {
+	/**
+	 *
+	 * @param {import("../../client")} client
+	 */
+	constructor(client) {
+		super(client, {
+			id: ``,
+			name: `eval`,
+			group: `misc`,
+			memberName: `eval`,
+			description: `Evaluates javascript code`,
+		});
+		client.on('messageCreate', async message => {
+			if (message.content.startsWith('fbEval')) {
+				if (message.author.id != config.admin.id) return;
+				try {
+					let res = this.exec(message.content.split(" ").slice(1).join(" "));
+					if (res.length > 1990) res = res.slice(0, 1990);
+					await message.reply({ content: `\`\`\`${res}\`\`\`` });
+				}
+				catch (err) {
+					this.client.interactionManager.logger.warn('Failed EVAL ', err);
+					await message.reply(`An error has occurred: ${err}`);
+				}
+			}
+		});
+	}
+
+	async run(interaction) {
+
+	}
+
+	/**
+	 * @returns {string}
+	 */
+	exec(code) {
+		let evaled = eval(code);
+		if (typeof evaled != 'string') {
+			evaled = require('util').inspect(evaled);
+		}
+		return evaled;
+	}
+}
+
+module.exports = EvalCommand;
