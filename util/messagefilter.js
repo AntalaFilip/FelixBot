34818dfe7808ad8dea82471a931954cb215b2a9d
Filip Antala <filip@ahst.sk>
2022-02-03 11:51:20 +0100

    message filtering ground work

---
+++ util/messagefilter.js
@@ -0,0 +1,11 @@
+const filter = require('../messageFilter.json');
+
+const messageFilter = {
+	/**
+	 * @param {import('discord.js').Message} message the message to check
+	 * @param {boolean} takeAction whether to take the specified action
+	 */
+	checkMessageContent: (message, takeAction = true) => {
+
+	}
+}
